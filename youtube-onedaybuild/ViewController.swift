21c714108e0564d4531d321059681f5c1576aac5
Ryoko815 <black.wadachi@gmail.com>
2020-05-20 19:42:23 +0900

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Ryoko Yokota on 2020/05/20.
+//  Copyright © 2020 Ryoko Yokota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
